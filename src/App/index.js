f3cf6e1a91cc88809af2a800ff3ff1cf092b5481
Jonathan Valencia <javalencis@gmail.com>
2022-08-24 14:56:53 -0500

    Almacenamiento en localStorage atraves de un Custom hook

--- src/App/index.js
+++ src/App/index.js
@@ -5,33 +5,72 @@ import { TodoItem } from '../components/TodoItem';
 import { TodoList } from '../components/TodoList';
 import { TodoSearch } from '../components/TodoSearch';
 
+function useLocalStorage(itemName,initialValue) {
+  const localStorageItem = localStorage.getItem(itemName)
+
+  let parsedItem;
+
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue))
+    parsedItem = initialValue
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = useState(parsedItem);
+
+  const saveItems = (newItems) => {
+    const stringifiedItems = JSON.stringify(newItems);
+    localStorage.setItem(itemName, stringifiedItems);
+    setItem(newItems)
+  }
+  return [item, saveItems]
+}
+
 function App() {
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1',[])
 
 
-
-
-  const [todos, setTodos] = useState([]);
   const [searchValue, setSearchValue] = useState('');
   const completedTodos = todos.filter(todo => todo.completed).length;
   const totalTodos = todos.length;
-  let todoList=[]
-  if (searchValue.length >= 1){
+  let todoList = []
+  if (searchValue.length >= 1) {
     todoList = todos.filter(todo => todo.text.toLowerCase().includes(searchValue.toLowerCase()))
-  }else{
+  } else {
     todoList = todos
   }
+  const onComplete = (text) => {
+    const indexText = todos.findIndex(t => t.text === text)
+    const newArray = [...todos]
+    newArray[indexText].completed = true;
+    saveTodos(newArray)
 
- 
+  }
+
+  const onDelete = (text) => {
+    const indexText = todos.findIndex(t => t.text === text)
+    const newArray = [...todos]
+    newArray.splice(indexText, 1);
+    saveTodos(newArray)
+
+  }
+
+
   return (
     <>
-      <TodoCounter 
+      <TodoCounter
         total={totalTodos}
-        completed = {completedTodos}
+        completed={completedTodos}
       />
       <TodoSearch setSearchValue={setSearchValue} />
       <TodoList>
         {todoList.map((todo, index) => {
-          return (<TodoItem key={index} todos ={todos} todo={todo} setTodos={setTodos}/>)
+          return (<TodoItem key={index}
+            todo={todo}
+            onComplete={onComplete}
+            onDelete={onDelete}
+          />)
         })}
       </TodoList>
       <CreateTodoButton />
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,32 +1,19 @@
 import './TodoItem.css'
 import { BsCheckLg } from 'react-icons/bs'
 import { CgClose } from 'react-icons/cg'
-export const TodoItem = ({todos,todo,setTodos}) => {
-  const onComplete = () => {
-    const indexText = todos.findIndex(t  => t.text === todo.text)
-    const newArray = [...todos]
-    newArray[indexText].completed = true;
-    setTodos(newArray)
-
-  }
-
-  const onDelete =()=>{
-    const indexText = todos.findIndex(t  => t.text === todo.text)
-    const newArray = [...todos]
-    newArray.splice(indexText,1);
-    setTodos(newArray)
-  
-  }
+export const TodoItem = ({todo,onComplete,onDelete}) => {
+ 
+ 
 
   return (
     <li className="TodoItem">
       <span 
         className={`Icon ${todo.completed && 'Icon-check'}`}
-        onClick={onComplete}><BsCheckLg /> </span>
+        onClick={()=>onComplete(todo.text)}><BsCheckLg /> </span>
       <p className={`TodoItem-p ${todo.completed && 'TodoItem-p--completed'}`}>{todo.text}</p>
       <span 
         className='Icon Icon-delete'
-        onClick={onDelete}><CgClose /></span>
+        onClick={()=>onDelete(todo.text)}><CgClose /></span>
     </li>
   )
 }
